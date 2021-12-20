ada2b9b8ad4cd36bfbe16490263d2e4cd3da15dc
tamnguy443 <60633642+tamnguy443@users.noreply.github.com>
2021-12-19 18:34:39 -0800

    Update MaximalSquare.java

--- MaximalSquare.java
+++ MaximalSquare.java
@@ -1,4 +1,6 @@
 /*
+December 17th 2021
+
 Given an m x n binary matrix filled with 0's and 1's, find the largest square containing only 1's and return its area.
 Input: matrix = [["1","0","1","0","0"],["1","0","1","1","1"],["1","1","1","1","1"],["1","0","0","1","0"]]
 Output: 4
