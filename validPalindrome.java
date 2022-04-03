3536c918ed7165d708de00bdb6988c8eb777ab09
Tam Nguyen <60633642+tamnguy443@users.noreply.github.com>
2022-04-02 17:15:38 -0700

    Create validPalindrome.java

---
+++ validPalindrome.java
@@ -0,0 +1,44 @@
+/*
+April 2nd 2022
+
+Given a string s, return true if the s can be palindrome 
+after deleting at most one character from it.
+
+Example 1:
+
+Input: s = "aba"
+Output: true
+Example 2:
+
+Input: s = "abca"
+Output: true
+Explanation: You could delete the character 'c'.
+*/
+class Solution {
+    public boolean validPalindrome(String s) {
+        int l = 0;
+        int r = s.length()-1;
+        while (l<=r){
+            if (s.charAt(l) == s.charAt(r)){
+                l++;
+                r--;
+            }
+            else{
+                return isPalindrome(s,l,r-1) || isPalindrome(s,l+1,r);
+            }
+        }
+        return true;
+    }
+    
+    private boolean isPalindrome(String str, int s, int t){
+        while (s <= t){
+            if (str.charAt(s) == str.charAt(t)){
+                s++;
+                t--;
+            }
+            else
+                return false;
+        }      
+        return true;
+    }
+}
